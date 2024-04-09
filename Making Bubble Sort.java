fde8ab9490dc1c0012b72485de98590cbac7f1e8
JorrinIkeno <151587314+JorrinIkeno@users.noreply.github.com>
2024-04-09 12:20:24 -1000

    Update Making Bubble Sort.java
    Errors in the for loop were fixed.

--- Making Bubble Sort.java
+++ Making Bubble Sort.java
@@ -1,5 +1,5 @@
 /**
- * This class is used to practice sorting.
+ * This class is used to practice bubble sorting.
  * 
  * @author Jorrin Ikeno
  */
@@ -44,20 +44,31 @@ public class Making_Bubble_Sort
 			
 			//Assign a random number to the current element of the array.
 			bigNums[i] = randomNumber;
-			
 		}
 
 		
 		//Call a method 'bubbleSort'.
-		bubbleSort(bigNums, len);
+		bubbleSort(bigNums);
 		
 		//Return the array.
 		return bigNums;
 	}
 	
-	
-	public static int[] bubbleSort(int[] arr, int length)
+	/**
+	 * This method should and return a totally new array
+	 * that is a sorted version of the passed array.
+	 * 
+	 * @param arr The passed array bigNums.
+	 * @param length The passed int len.
+	 * @return
+	 */
+	public static int[] bubbleSort(int[] arr)
 	{
+		//int length = arr.length;
+		
+		//Create a copy of the array.
+		int[] copyArr = arr;
+		
 		//Declare a traverser int.
 		int traverser = 0;
 		
@@ -65,60 +76,43 @@ public class Making_Bubble_Sort
 		int placeholder = 0;
 		
 		//Traverse the array.
-		for(; traverser < arr.length - 1; traverser++)
+		for(; traverser <= copyArr.length - 1; traverser++)
 		{
-			/* 
-			 * Create an if statement to account for
-			 * if the current element is greater than
-			 * the next element.
-			 */
-			if(arr[traverser] >= arr[traverser + 1])
+			//Create a nested for loop to sort the array again.
+			for(int traverser2 = 0; traverser2 < copyArr.length - 1; traverser2++)
 			{
-				//Set 'placeholder' equal to the next element.
-				placeholder = arr[traverser + 1];
-				
-				//Swap the next element with the current element.
-				arr[traverser + 1] = arr[traverser];
-				
-				//Swap the current element and 'placeholder'.
-				arr[traverser] = placeholder;
-				
-				//Create a nested for loop to sort the array again.
-				for(int traverser2 = 0; traverser2 < arr.length - 1; traverser2++)
+				/* 
+				 * Create an if statement to account for
+				 * if the current element is greater than
+				 * the next element.
+				 */
+				if(copyArr[traverser2] >= copyArr[traverser2 + 1])
 				{
-					/* 
-					 * Create an if statement to account for
-					 * if the current element is greater than
-					 * the next element.
-					 */
-					if(arr[traverser2] >= arr[traverser2 + 1])
-					{
-						//Set 'placeholder' equal to the next element.
-						int placeholder2 = arr[traverser2 + 1];
+					//Set 'placeholder' equal to the next element.
+					int placeholder2 = copyArr[traverser2 + 1];
 						
-						
-						//Swap the next element with the current element.
-						arr[traverser2 + 1] = arr[traverser2];
+					//Swap the next element with the current element.
+					copyArr[traverser2 + 1] = copyArr[traverser2];
 					
-						//Swap the current element and 'placeholder'.
-						arr[traverser2] = placeholder2;
-					}
+					//Swap the current element and 'placeholder'.
+					copyArr[traverser2] = placeholder2;
 				}
 			}
+			
 		}
 		
 		//Call a method 'printArr'.
-		printArr(arr);
+		printArr(copyArr);
 		
 		//Return the arr.
-		return arr;
+		return copyArr;
 	}
 	
 	/**
 	 * This method is passed an array and prints out each
 	 * element one by one.
 	 * 
-	 * @param arr The passed array nums.
+	 * @param arr The passed array.
 	 */
 	public static void printArr(int[] arr)
 	{
